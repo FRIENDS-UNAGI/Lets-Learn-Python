33e82f4a20a11bd9b395506beb24a6a307066559
Mohamed Shabeer K P <mohammedshabeerkp@gmail.com>
2024-02-04 16:00:13 +0530

    Create assignment-arthematic-operations.py

---
+++ assignment-arthematic-operations.py
@@ -0,0 +1,7 @@
+x = int(input("enter first number"))
+y = int(input("enter second number"))
+A = x+y
+S = x-y
+M = x*y
+D = x/y
+print("Sum of these numbers is" , A , ",on substracting it is" , S , ",by multiplying it's" , M ,",and on dividing we get" , D)
